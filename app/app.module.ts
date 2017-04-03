2c91c86fafa12be413b240cf8d4512f1c230a009
NStephenson <nickstephenson90@gmail.com>
2017-04-03 12:36:18 -0400

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DocumentsComponent } from './documents/documents.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    DocumentsComponent,
+    HomepageComponent,
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule{}
