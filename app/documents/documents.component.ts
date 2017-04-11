4fb35b7171164d387bb55060643bcafac529a559
NStephenson <nickstephenson90@gmail.com>
2017-04-10 20:40:11 -0400

    added bootstrap styling

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
       description: "This is a document",
       file_url: "https://google.com",
       updated_at: "11/11/16",
-      image_url: "htttps://google.com"
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/2010/12/puppies.jpg"
     },
     {
       title: "My Second Doc",
       description: "This is a document too",
       file_url: "https://google.com",
       updated_at: "11/12/16",
-      image_url: "htttps://google.com"
+      image_url: "http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg"
     },
     {
       title: "My Third Doc",
       description: "This is a document trice",
       file_url: "https://google.com",
       updated_at: "11/4/16",
-      image_url: "htttps://google.com"
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg"
     }
   ]
 } 
