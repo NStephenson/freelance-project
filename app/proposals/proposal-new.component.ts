f423de023bc6c748b3522693b8e1889f5f82d2d5
NStephenson <nickstephenson90@gmail.com>
2017-05-25 01:02:21 -0400

    added form submission functionality

--- app/proposals/proposal-new.component.ts
+++ app/proposals/proposal-new.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core'
-import { Proposal } from './proposal'
+import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+import { ProposalService} from './proposal.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   moduleId: module.id,
   selector: 'propsal-new',
   templateUrl: 'proposal-new.component.html',
-  styleUrls: ['proposal-new.component.css']
+  styleUrls: ['proposal-new.component.css'],
+  providers: [ProposalService]
 })
 
 export class ProposalNewComponent {
   proposal: Proposal = new Proposal;
   submitted: boolean = false;
+
+  constructor(
+    private proposalService: ProposalService
+  ){}
+
+  createProposal(proposal){
+    this.submitted = true;
+    this.proposalService.createProposal(proposal)
+        .subscribe(data => {return true;}, 
+                   error => { console.log("error saving");
+                              return Observable.throw(error);
+                            });
+  }
+
 }
+
